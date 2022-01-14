93c78190ce0ec97a0f023df089634eef12b6d28a
Mesach Venegas <mesachman@gmail.com>
2022-01-13 19:41:37 -0800

    refactorizacion de codigo, despliegue de campos del cliente.

--- main_window.py
+++ main_window.py
@@ -1,7 +1,8 @@
-from tkinter import  PhotoImage, Tk, messagebox, ttk
+from tkinter import  PhotoImage, Tk, messagebox, scrolledtext, ttk
 from search_window import SearchWin
 from NewClient_window import NewClient
 from Module.client import Client
+from Module.notas import Notas
 from Module.dao import DaoClient, DaoNotas, DaoProduct
 from Module.log_gen import log
 import tkinter as tk
@@ -19,14 +20,27 @@ class MainWindow(Tk):
         del_img = os.path.abspath("Sources/images/delete_client.png")
         prod_img = os.path.abspath("Sources/images/prods_64x64.png")
         clear_img = os.path.abspath("Sources/images/clear_btn.png")
+        see_img = os.path.abspath("Sources/images/ver_32x32.png")
+        del_note_img = os.path.abspath("Sources/images/delete_32x32.png")
+        new_note_img = os.path.abspath("Sources/images/add_note_32x32.png")
+        edit_note_img = os.path.abspath("SOurces/images/edit_note_32x32.png")
+        save_note_img = os.path.abspath("Sources/images/save_file.png")
         self._new = PhotoImage(file= add_img)
         self._search = PhotoImage(file= search_img)
         self._edit = PhotoImage(file= edit_img)
         self._delete = PhotoImage(file= del_img)
         self._prod = PhotoImage(file= prod_img)
         self._clean = PhotoImage(file= clear_img)
+        self._see = PhotoImage(file= see_img)
+        self._del = PhotoImage(file= del_note_img)
+        self._add_note = PhotoImage(file= new_note_img)
+        self._edit_note = PhotoImage(file= edit_note_img)
+        self._save_note = PhotoImage(file= save_note_img)
         # Variables.
         self._client = None
+        self._head_notas = []
+        self._cont_note = []
+        self._content = None
         self._name = tk.StringVar(value= None)
         self._clave = tk.StringVar(value= None)
         self._lastname = tk.StringVar(value= None)
@@ -37,6 +51,8 @@ class MainWindow(Tk):
         self._type = tk.StringVar(value= None)
         self._location = tk.StringVar(value= None)
         self._date = tk.StringVar(value= None)
+        self._title = tk.StringVar(value= None)
+        self._note_content = tk.StringVar(value= None)
         # Dimensiones de la ventana.
         width = 900
         height = 540
@@ -50,15 +66,17 @@ class MainWindow(Tk):
         self.resizable(1,1)
         self.state('zoomed')
         self.wm_minsize(width, height)
-        self._main_frame = tk.Frame(self)
-        self._main_frame.pack(fill='both', anchor='ne')
+        # Propiedades del grid.
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=2)
         self._side_buttons()
         self._data_client()
         self.mainloop()
 
     def _side_buttons(self):
         # Frame para la botonera de opciones del app.
-        btn_frame = tk.Frame(self._main_frame, relief=tk.RAISED, border=2)
+        btn_frame = tk.Frame(self, relief=tk.RAISED, border=2)
         btn_frame.grid(row=0, column=0, sticky="NSEW")
         # Botones.
         # Agregar un cliente nuevo.
@@ -134,14 +152,45 @@ class MainWindow(Tk):
         )
         self.prods_btn.grid(row=5, column=0, padx=5, pady=10, sticky="NSEW")
 
-    def _data_client(self):
+    def _data_client(self, cliente: Client = None):
         # Frame donde se cargaran los datos del cliente seleccionado.
-        self.data_frame = tk.LabelFrame(self._main_frame, text="Cliente")
+        self.data_frame = tk.LabelFrame(self, text="Cliente")
         self.data_frame.grid(row=0, column=1, padx=5, pady=10, sticky="NSEW")
-        # Logo o imagen del cliente.
-        logo = tk.Label(self.data_frame, text="Not Aviable",bg="#fff")
-        logo.config(height=6, width=16)
-        logo.grid(row=0, column=0, padx=5, pady=10, rowspan=3)
+        # Si ya se selecciono un cliente se cargaran sus datos en las variables.
+        if cliente:
+            # Logo del cliente.
+            logo_img = os.path.abspath(cliente.image)
+            self._logo = PhotoImage(file= logo_img)
+            # Variables.
+            self._clave = tk.StringVar(value= cliente.clave)
+            self._name = tk.StringVar(value= cliente.name)
+            self._lastname = tk.StringVar(value= cliente.lastname)
+            self._mothers = tk.StringVar(value= cliente.mothers)
+            self._phone = tk.StringVar(value= cliente.phone)
+            self._debt = tk.DoubleVar(value= cliente.debt)
+            self._balance = tk.DoubleVar(value= cliente.balance)
+            self._date = tk.StringVar(value= cliente.date)
+            self._type = tk.StringVar(value= cliente.type_client)
+            self._location = tk.StringVar(value= cliente.location)
+            # Logo o imagen del cliente.
+            logo = tk.Label(self.data_frame, image= self._logo, compound='center' ,bg="#fff")
+            logo.config(height=120, width=120)
+            logo.grid(row=0, column=0, padx=5, pady=10, rowspan=3)
+            new_state = 'active'
+            see_state = 'active'
+            del_state = 'active'
+            edit_state = 'active'
+            save_state = 'active'
+        else:
+            # Logo o imagen del cliente.
+            logo = tk.Label(self.data_frame, text="Not Aviable",bg="#fff")
+            logo.config(height=6, width=16)
+            logo.grid(row=0, column=0, padx=5, pady=10, rowspan=3)
+            new_state = 'disabled'
+            see_state = 'disabled'
+            del_state = 'disabled'
+            edit_state = 'disabled'
+            save_state = 'disabled'
         # Datos personales del cliente.
         # Nombre(s).
         l_name = tk.Label(self.data_frame, text="Nombre(s)", font=('arial',10,'bold'), justify='right')
@@ -232,7 +281,7 @@ class MainWindow(Tk):
         self._e_balance.grid(row=5, column=3, padx=2, pady=5, sticky="NW")
         # Separador
         sep = ttk.Separator(self.data_frame, orient='horizontal')
-        sep.grid(row=6, column=0, padx=3, sticky='EW', columnspan=4)
+        sep.grid(row=6, column=0, padx=3,pady=5, sticky='EW', columnspan=4)
         # carga de notas.
         lab = tk.Label(
             self.data_frame,
@@ -250,6 +299,89 @@ class MainWindow(Tk):
         # Dimensiones y configuración de la columna.
         self.tabla_notas.column("titulo", anchor='center')
         self.tabla_notas.column("fecha", width=70, anchor='center')
+        # Scrollbar para la info de las notas
+        scrollbar = ttk.Scrollbar(self.data_frame, orient=tk.VERTICAL, command=self.tabla_notas.yview)
+        self.tabla_notas.configure(yscrollcommand=scrollbar.set)
+        scrollbar.grid(row=8, column=4, sticky="NS")
+        # Carga de la info de las notas
+        self._notes_table(self._clave.get())
+        # Botones de interacion.
+        self.see_btn = ttk.Button(self.data_frame, text="Ver", image= self._see, compound='left', state= see_state)
+        self.see_btn.grid(row=9, column=3, padx=5, pady=5, sticky='NW')
+        # Visualización de la nota.
+        # Titulo.
+        note_title = tk.Label(self.data_frame, text="Titulo", justify='right')
+        note_title.grid(row=10, column=0,  padx=5, pady=5 , sticky="NE")
+        self.e_note = ttk.Entry(self.data_frame, textvariable= self._title, width=30, font=('arial',10,'bold'), justify='left', state=tk.DISABLED)
+        self.e_note.grid(row=10, column=1, padx=5, pady=5, sticky='NEW', columnspan=2)
+        # Contenido de la nota.
+        self.note_box = scrolledtext.ScrolledText(
+            self.data_frame,
+            font = ('arial',10,'bold'),
+            height = 8,
+            width = 80,
+            wrap = tk.WORD,
+            state=tk.DISABLED,
+            border = 0
+        )
+        self.note_box.grid(row=11, column=0, padx=5, pady=7, columnspan=4)
+        # Boton de nueva nota.
+        new_note = ttk.Button(
+            self.data_frame,
+            text="Nueva",
+            image= self._add_note,
+            compound='left',
+            state= new_state
+        )
+        new_note.grid(row=12, column=0, padx=5, pady=5, sticky='EW')
+        # Boton para eliminar una nota
+        del_btn = ttk.Button(
+            self.data_frame,
+            text="Eliminar",
+            image= self._del,
+            compound='left',
+            state= del_state
+        )
+        del_btn.grid(row=12, column=1, padx=5, pady=5, sticky='EW')
+        # Boton para editar la nota.
+        edit_btn = ttk.Button(
+            self.data_frame,
+            text="Editar",
+            image= self._edit_note,
+            compound='left',
+            state= edit_state
+        )
+        edit_btn.grid(row=12, column=2, padx=5, pady=5, sticky='EW')
+        # Boton para guardar la nota
+        save_btn = ttk.Button(
+            self.data_frame,
+            text="Guardar",
+            image= self._save_note,
+            compound='left',
+            state= save_state
+        )
+        save_btn.grid(row=12, column=3, padx=5, pady=5, sticky='EW')
+
+    def _notes_table(self, key: str):
+        if self._head_notas:
+            self._head_notas = []
+            it = self.tabla_notas.get_children()
+            for item in it:
+                self.tabla_notas.delete(item)
+        if key:
+            notes = DaoNotas.getNotas(key)
+            if notes:
+                for note in notes:
+                    info = Notas(
+                        id = note[0],
+                        id_nota= note[1],
+                        f_ingreso= note[2],
+                        titulo= note[3],
+                        nota= note[4]
+                    )
+                    self._cont_note.append(info)
+                    self._head_notas = [info.titulo, info.f_ingreso]
+                    self.tabla_notas.insert("", tk.END, values= self._head_notas)
 
     def _load_client(self):
         search = SearchWin()
@@ -275,135 +407,7 @@ class MainWindow(Tk):
             self.delete_btn.config(state='active')
             self.clean_btn.config(state='active')
             self.prods_btn.config(state='active')
-            self._data_in(self._client)
-
-    def _data_in(self, cliente: Client):
-        # Variables.
-        self._clave = tk.StringVar(value= cliente.clave)
-        self._name = tk.StringVar(value= cliente.name)
-        self._lastname = tk.StringVar(value= cliente.lastname)
-        self._mothers = tk.StringVar(value= cliente.mothers)
-        self._phone = tk.StringVar(value= cliente.phone)
-        self._debt = tk.DoubleVar(value= cliente.debt)
-        self._balance = tk.DoubleVar(value= cliente.balance)
-        self._date = tk.StringVar(value= cliente.date)
-        self._type = tk.StringVar(value= cliente.type_client)
-        self._location = tk.StringVar(value= cliente.location)
-        # ubicación de la imagen.
-        client_img = os.path.abspath(cliente.image)
-        self._logo = PhotoImage(file= client_img)
-        # Logo o imagen del cliente.
-        logo = tk.Label(self.data_frame, image= self._logo, compound='center', bg="#fff")
-        logo.config(height=120, width=120)
-        logo.grid(row=0, column=0, padx=5, pady=10, sticky="NSEW", rowspan=3)
-        # Datos personales del cliente.
-        # Nombre(s).
-        l_name = tk.Label(self.data_frame, text="Nombre(s)", font=('arial',10,'bold'), justify='right')
-        l_name.grid(row=0, column=1, padx=5, pady=5, sticky='NE')
-        self._e_name = ttk.Entry(
-            self.data_frame,
-            textvariable= self._name,
-            font=('arial',10,'bold'),
-            state= tk.DISABLED,
-            width=25)
-        self._e_name.grid(row=0, column=2, padx=10,pady=5, sticky="NW", columnspan=2)
-        # Apellido Paterno.
-        l_lastname = tk.Label(self.data_frame, text='A.Paterno', font=('arial',10,'bold'), justify='right')
-        l_lastname.grid(row=1, column=1, padx=5, pady=5, sticky='NE')
-        self._e_lastname = ttk.Entry(
-            self.data_frame,
-            textvariable= self._lastname,
-            font=('arial',10,'bold'),
-            state= tk.DISABLED,
-            width=25
-        )
-        self._e_lastname.grid(row=1, column=2, padx=10,pady=5, sticky="NW", columnspan=2)
-        # Apellido materno.
-        l_mothers = tk.Label(self.data_frame, text='A.Materno', font=('arial',10,'bold'), justify='right')
-        l_mothers.grid(row=2, column=1, padx=5, pady=5, sticky='NE')
-        self._e_mothers = ttk.Entry(
-            self.data_frame,
-            textvariable= self._mothers,
-            font=('arial',10,'bold'),
-            state= tk.DISABLED,
-            width=25
-        )
-        self._e_mothers.grid(row=2, column=2, padx=10,pady=5, sticky="NW", columnspan=2)
-        # Telefono.
-        l_phone = tk.Label(self.data_frame, text="Telefono", font=('arial',10,'bold'), justify='right')
-        l_phone.grid(row=3, column=1, padx=5, pady=5, sticky="NE")
-        self._e_phone = ttk.Entry(
-            self.data_frame,
-            textvariable= self._phone,
-            font=('arial',10,'bold'),
-            state= tk.DISABLED,
-            width=25
-        )
-        self._e_phone.grid(row=3, column=2, padx=10,pady=5, sticky="NW", columnspan=2)
-        # Tipo de cliente.
-        l_type = tk.Label(self.data_frame, text="Tipo de Cliente", font=('arial',10,'bold'), justify='right')
-        l_type.grid(row=4, column=0, padx=5, pady=5, sticky="NW")
-        self._e_type = ttk.Entry(
-            self.data_frame,
-            textvariable= self._type,
-            font=('arial',10,'bold'),
-            state= tk.DISABLED,
-            width=15
-        )
-        self._e_type.grid(row=4, column=1, padx=2, pady=5, sticky="NW")
-        # Localización del cliente.
-        l_local = tk.Label(self.data_frame, text="Localización", font=('arial',10,'bold'), justify='right')
-        l_local.grid(row=5, column=0, padx=5, pady=5, sticky="NW")
-        self._e_type = ttk.Entry(
-            self.data_frame,
-            textvariable= self._location,
-            font=('arial',10,'bold'),
-            state= tk.DISABLED,
-            width=15
-        )
-        self._e_type.grid(row=5, column=1, padx=2, pady=5, sticky="NW")
-        # Adeudo.
-        l_debt = tk.Label(self.data_frame, text="Adeudo", font=('arial',10,'bold'), justify='right')
-        l_debt.grid(row=4, column=2, padx=5, pady=5, sticky="NW")
-        self._e_debt = ttk.Entry(
-            self.data_frame,
-            textvariable= self._debt,
-            font=('arial',10,'bold'),
-            state= tk.DISABLED,
-            width=15
-        )
-        self._e_debt.grid(row=4, column=3, padx=2, pady=5, sticky="NW")
-        # Balance a favor.
-        l_balance = tk.Label(self.data_frame, text="A Favor", font=('arial',10,'bold'), justify='right')
-        l_balance.grid(row=5, column=2, padx=5, pady=5, sticky="NW")
-        self._e_balance = ttk.Entry(
-            self.data_frame,
-            textvariable= self._balance,
-            font=('arial',10,'bold'),
-            state= tk.DISABLED,
-            width=15
-        )
-        self._e_balance.grid(row=5, column=3, padx=2, pady=5, sticky="NW")
-        # Separador
-        sep = ttk.Separator(self.data_frame, orient='horizontal')
-        sep.grid(row=6, column=0, padx=3, sticky='EW', columnspan=4)
-        # carga de notas.
-        lab = tk.Label(
-            self.data_frame,
-            text="Notas",
-            font=('arial',10,'bold'),
-            anchor='nw'
-        )
-        lab.grid(row=7, column=0, padx=5, sticky="EW", columnspan=4)
-        # Tabla con las notas.
-        self.tabla_notas = ttk.Treeview(self.data_frame, columns=("titulo","fecha"), show='headings', height=4)
-        self.tabla_notas.grid(row=8, column=0,padx=7, sticky='NSEW', columnspan=4)
-        # Nombre de las columnas.
-        self.tabla_notas.heading("titulo", text="Titulo")
-        self.tabla_notas.heading("fecha", text="Fecha de ingreso")
-        # Dimensiones y configuración de la columna.
-        self.tabla_notas.column("titulo", anchor='center')
-        self.tabla_notas.column("fecha", width=70, anchor='center')
+            self._data_client(self._client)
 
     def _edit_client(self):
         pass
@@ -413,6 +417,12 @@ class MainWindow(Tk):
 
     def _load_prods(self):
         pass
+
+    def _load_content(self, id:int):
+        if id:
+            print(id)
+            # note = self._cont_note[id]
+            # print(note)
 
     def _clean_data(self):
         self._name = tk.StringVar(value= None)

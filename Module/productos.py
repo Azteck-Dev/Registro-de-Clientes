22d3bdc572ccfcef638b97f8fb12ce8379710529
Mesach Venegas <mesachman@gmail.com>
2022-01-16 19:51:08 -0800

    ajustes de informacion del producto y su clase

--- Module/productos.py
+++ Module/productos.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 
 class Producto:
-    def __init__(self, id:str = None, folio: int = None, name:str = None, description:str = None, cost:float = 0, f_in:str = None, f_out: str = None, prod_id:str = None, cantidad:int = None):
+    def __init__(self, id:str = None, folio: int = None, name:str = None, description:str = None, cost:float = 0, f_in:str = None, f_out: str = None, prod_id:str = None, cantidad:int = None, size:str = None, currency:float = None):
         self._id = id
         self._folio = folio
         self._name = name
         self._description = description
         self._cantidad = cantidad
+        self._size = size
         self._cost = cost
+        self._currency = currency
         self._f_in = f_in
         self._f_out = f_out
         self._prod_id = self._prod_code(prod_id)
@@ -42,6 +44,18 @@ class Producto:
     @cantidad.setter
     def cantidad(self, value):
         self._cantidad = value
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self, value):
+        self._size = value
+    @property
+    def currency(self):
+        return self._currency
+    @currency.setter
+    def currency(self, value):
+        self._currency = value
 
 
     def _prod_code(self, prod_id:str):
@@ -70,14 +84,14 @@ Folio: {self._folio}
 Prod ID: {self._prod_id}
 Nombre: {self._name}
 Descripcion: {self._description}
-Cantidad: {self._cantidad}
-Costo: {self._cost}
+Cantidad: {self._cantidad} {self._size}
+Costo: {self._cost} {self._currency}
 Ingreso: {self._f_in}
 Salida: {self._f_out}
     '''
 
 if __name__ == '__main__':
-    prod = Producto('CN-1702JHVS8242','7044','Jalapeño', 'Chile verde',11.20)
+    prod = Producto('CN-1702JHVS8242','7044','Jalapeño', 'Chile verde',11.20,currency="mxn",size="kgs", cantidad=10)
     prod.dateInOut('in')
     prod.dateInOut('out')
     print(prod)
